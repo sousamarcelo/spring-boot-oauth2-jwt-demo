b70201bd55c6b99e37cc1016f61061194cd7aea7
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-05 19:27:29 -0300

    BCrypt password encoder adicionado para criptografia de senhas

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -5,9 +5,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	
+	/* Teste
+	@Autowired
+	private PasswordEncoder passwordEncoder; //como la na classe "SecurityConfig " foi utilizado a annotation "Bean"(componente) o proprio framword vai trazer a implementação(instancia) do BCryptPasswordEncoder[encripator])
+	*/
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	/* Teste
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("ENCODE = " + passwordEncoder.encode("123456")); //testando criptografia de senha com BCryptPasswordEncoder injetado aqui para testes.
+			//RETORNO NO CONSOLE: ENCODE = $2a$10$hgmM74oeD7n30MD0Sbo/VOEoBhbsMagczJIsk9XaxmFS7UHsrtxCG
+		
+		//isso é o que o framework faz por traz para validar se senha digitada pelo usuario é a mesma criptografada
+		boolean result = passwordEncoder.matches("123456", "$2a$10$hgmM74oeD7n30MD0Sbo/VOEoBhbsMagczJIsk9XaxmFS7UHsrtxCG");
+		System.out.println("RESULTADO IGUAL = " + result);
+			//RETORNO NO CONSOLE: RESULTADO IGUAL = true
+	}
+	*/
+	
+	
+	
 
 }
--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,13 +3,21 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 	//CONFIGURAÇÃO GLOBAL
 	
+	
 	@Bean
+	public PasswordEncoder getPasswordEncoder() { //Metodo de criptografica de senha, por exemplo a senha de usuario, ao inserir no banco será criptografada. foi utilizado a interfeca para não expor a implementação, essa é instanciada no retorno logo abaixo, exemplo de uma implementação dessa interface --> "BCryptPasswordEncoder" que aplica o algoritimo do decript
+		return new BCryptPasswordEncoder();
+	} 
+	
+	@Bean //indicação para componente de sistema
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable()); //csrf(Ataque csrf) --> DESABILITADO PROTEÇÃO/SEGURAÇÃO DE GRAVAÇÃO DE DADOS NA SEÇÃO, API REST NÃO GRAVA DADO EM SEÇÃO
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); //[.permitAll()] --> ESSE É UM CONTROLE GLOBAL E ESTÁ TUDO LIBERADO PORQUE O CONTROLE/SEGURANÇA SERÁ POR ROTA. CONFIGURAÇÃO DA PERMISSÃO PARA OS ENDPOINS/REQUISIÇÃO. PEMITE ALL. [.permitAll()] PODE SER ALTERADO PARA PERTIRI ACESSAR SOMENTE QUE UM PERFIL ESPECIFICO, ALEM DE OUTRAS OPÇÕES DE CONTROLE DE ACESSO APENAS MUDANDO ESSE METODO
