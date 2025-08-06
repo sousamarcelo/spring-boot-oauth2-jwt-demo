bae58ff7d3b5b71ddb91cc03f044a25521886fb0
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-06 20:32:23 -0300

    'Projection UserDatailsProjection' criada

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
@@ -1,16 +1,25 @@
 package com.devsuperior.demo.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.demo.entities.User;
+import com.devsuperior.demo.repositories.UserRepository;
+
 @Service
 public class UserService implements UserDetailsService {
-
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
+		
+		User user = userRepository.findByEmail(username);
+		
 		return null;
 	}
 
