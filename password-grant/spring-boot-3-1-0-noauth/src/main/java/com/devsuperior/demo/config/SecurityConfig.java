3c5f82e5610bd8a94bedeb9f3fdbf357dafe3b12
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-06 20:23:38 -0300

    inserido filtro do h2 no metodo 'h2SecurityFilterChain' da classe config com anotation @Order(1)

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.demo.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,7 +20,18 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	} 
 	
+	@Bean
+	@Profile("test")
+	@Order(1)
+	SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+
+		http.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
+				.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+		return http.build();
+	}
+	
 	@Bean //indicação para componente de sistema
+	@Order(2) // order 2 para que rode depois da aplicação do filtro do h2
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable()); //csrf(Ataque csrf) --> DESABILITADO PROTEÇÃO/SEGURAÇÃO DE GRAVAÇÃO DE DADOS NA SEÇÃO, API REST NÃO GRAVA DADO EM SEÇÃO
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); //[.permitAll()] --> ESSE É UM CONTROLE GLOBAL E ESTÁ TUDO LIBERADO PORQUE O CONTROLE/SEGURANÇA SERÁ POR ROTA. CONFIGURAÇÃO DA PERMISSÃO PARA OS ENDPOINS/REQUISIÇÃO. PEMITE ALL. [.permitAll()] PODE SER ALTERADO PARA PERTIRI ACESSAR SOMENTE QUE UM PERFIL ESPECIFICO, ALEM DE OUTRAS OPÇÕES DE CONTROLE DE ACESSO APENAS MUDANDO ESSE METODO
