761cf487dc522e5ebe03d563635b09a184485532
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-06 20:51:29 -0300

    Implementando o checklist do Spring Security PARTE 2, not tested

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/services/UserService.java
@@ -1,12 +1,16 @@
 package com.devsuperior.demo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.demo.entities.Role;
 import com.devsuperior.demo.entities.User;
+import com.devsuperior.demo.projections.UserDetailsProjection;
 import com.devsuperior.demo.repositories.UserRepository;
 
 @Service
@@ -18,9 +22,18 @@ public class UserService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
-		User user = userRepository.findByEmail(username);
+		List<UserDetailsProjection>	result = userRepository.searchUserAndRolesByEmail(username);
+		if (result.size() == 0) {
+			throw new UsernameNotFoundException("User not found");
+		}
 		
-		return null;
+		//montando um objeto usuario que as permissões/roles, dessa forma evitar o problema do lazy da jpa e evita ter que alterar a consulta para .EAGUER que nesse caso que é uma relação muitos para muitos deixando lento
+		User user = new User();
+		user.setEmail(username);
+		user.setPassword(result.get(0).getPassword());
+		for(UserDetailsProjection projection : result) { //pegando as regras do usuarios
+			user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		return user;
 	}
-
 }
